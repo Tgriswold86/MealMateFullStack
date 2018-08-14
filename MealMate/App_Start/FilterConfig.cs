e2c66b88036c5877ef57225dc68eca3a41573411
Tim Griswold <tgriswold86@gmail.com>
2018-08-14 10:33:35 -0400

    Add project files.

---
+++ MealMate/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MealMate
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
